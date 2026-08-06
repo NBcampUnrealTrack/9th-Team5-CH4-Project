464e1eee8c0c3793da673624b8cbe2e20c02c3a4
Shin Dain <qwe3546@naver.com>
2026-08-06 21:03:53 +0900

    feat : ItemDefinition, ItemInstance 추가

---
+++ Source/DeepRaiders/Item/DRItemDefinition.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DRItemDefinition.generated.h"
+
+UENUM(BlueprintType)
+enum class EItemCategory : uint8
+{
+	Ore,
+	Equipment,
+	Consumable,
+	End,
+};
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class DEEPRAIDERS_API UDRItemDefinition : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item")
+	FName ItemId;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item")
+	EItemCategory Category;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item")
+	FText DisplayName;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item")
+	FText Description;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item", meta = (ClampMin = 1, UIMin = 1))
+	int32 MaxStackSize = 1;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item | Trade")
+	uint8 bCanBeSold:1 = false;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item | Trade", meta = (ClampMin = 1, UIMin = 1))
+	int32 Price = 0;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Item")
+	TObjectPtr<UStaticMesh> WorldMesh;
+};
+
