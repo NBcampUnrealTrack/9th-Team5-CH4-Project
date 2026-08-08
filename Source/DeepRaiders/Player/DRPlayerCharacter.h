9ad6620aad57a00d162040366681870bc7fbc053
SeungMinLee <mealh333@gmail.com>
2026-08-08 10:42:09 +0900

    feat: 플레이어별 제트팩 보유 상태 및 외형 복제 기반 구현
    플레이어별 제트팩 보유 상태와 연료를 DRPlayerState에서 관리
    제트팩 보유 여부와 연료 복제
    제트팩 획득 시 서버 및 클라이언트 외형 갱신
    Character의 등 소켓에 제트팩 메시 표시
    스폰 직후 GrantJetpack()을 호출하는 임시 테스트

--- Source/DeepRaiders/Player/DRPlayerCharacter.h
+++ Source/DeepRaiders/Player/DRPlayerCharacter.h
@@ -26,11 +26,21 @@ class DEEPRAIDERS_API ADRPlayerCharacter : public ACharacter
 public:
     ADRPlayerCharacter();
 
+    virtual void Tick(float DeltaSeconds) override;
+    virtual void Landed(const FHitResult& Hit) override;
+
+    /** 지상에서는 점프, 공중에서는 제트팩 사용을 요청한다. */
+    void HandleJumpPressed();
+
+    /** 점프 입력과 제트팩 사용을 종료한다. */
+    void HandleJumpReleased();
+    
     /** 로컬 플레이어의 채굴 요청을 MiningComponent에 전달한다. */
     void RequestMine();
     
     virtual void PossessedBy(AController* NewController) override;
     virtual void OnRep_Controller() override;
+    virtual void OnRep_PlayerState() override;
 
     virtual void GetLifetimeReplicatedProps(
         TArray<FLifetimeProperty>& OutLifetimeProps) const override;
@@ -52,6 +62,16 @@ public:
 
     /** 현재 등 장비 외형을 제거한다. */
     void ClearBackEquipmentVisual();
+    
+    /*
+     *  제트팩 외형을 적용한다
+     *  적용 시점은 아래와 같음
+     *  PossessedBy
+     *  OnRep_PlayerState
+     *  제트팩 획득 직후 서버
+     *  PlayerState 복제 수신 직후 클라이언트
+     */
+    void RefreshJetpackVisual();
 
     void MoveInput(const FVector2D& MoveInput);
     void LookInput(const FVector2D& LookInput);
@@ -83,6 +103,24 @@ private:
     UFUNCTION()
     void OnRep_NetworkTestActive();
 
+    UFUNCTION(Server, Reliable)
+    void ServerStartJetpack();
+
+    UFUNCTION(Server, Reliable)
+    void ServerStopJetpack();
+
+    UFUNCTION()
+    void OnRep_JetpackActive();
+
+    bool CanStartJetpack() const;
+
+    void StartJetpackFromServer();
+    void StopJetpackFromServer();
+
+    void UpdateJetpack(float DeltaSeconds);
+
+    void RefreshJetpackActivePresentation();
+    
 protected:
     UPROPERTY(
         VisibleAnywhere,
@@ -146,4 +184,46 @@ protected:
         EditDefaultsOnly,
         Category = "Player|Equipment|Test")
     FTransform TestWorldBackTransform;
+    
+    UPROPERTY(
+        EditDefaultsOnly,
+        Category = "Player|Equipment|Jetpack")
+        TObjectPtr<UStaticMesh> JetpackMesh;
+    
+    UPROPERTY(
+        EditDefaultsOnly,
+        Category = "Player|Equipment|Jetpack")
+    FTransform JetpackRelativeTransform;
+    
+    /** 현재 Pawn이 실제로 제트팩을 분사 중인지 나타낸다. */
+    UPROPERTY(
+        ReplicatedUsing = OnRep_JetpackActive,
+        VisibleAnywhere,
+        BlueprintReadOnly,
+        Category = "Player|Jetpack")
+    bool bIsJetpackActive = false;
+
+    /** 초당 상승 가속도 */
+    UPROPERTY(
+        EditDefaultsOnly,
+        BlueprintReadOnly,
+        Category = "Player|Jetpack",
+        meta = (ClampMin = "0.0", Units = "cm/s^2"))
+    float JetpackAcceleration = 2500.f;
+
+    /** 제트팩 사용 중 최대 상승 속도 */
+    UPROPERTY(
+        EditDefaultsOnly,
+        BlueprintReadOnly,
+        Category = "Player|Jetpack",
+        meta = (ClampMin = "0.0", Units = "cm/s"))
+    float MaxJetpackRiseSpeed = 900.f;
+
+    /** 초당 연료 소비량 */
+    UPROPERTY(
+        EditDefaultsOnly,
+        BlueprintReadOnly,
+        Category = "Player|Jetpack",
+        meta = (ClampMin = "0.0"))
+    float JetpackFuelConsumptionPerSecond = 20.f;
 };
---
+++ Source/DeepRaiders/Player/DRPlayerState.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "DRPlayerState.generated.h"
+
+class FLifetimeProperty;
+
+UCLASS()
+class DEEPRAIDERS_API ADRPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	virtual void GetLifetimeReplicatedProps(
+		TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	bool HasJetpack() const
+	{
+		return bHasJetpack;
+	}
+
+	float GetJetpackFuel() const
+	{
+		return CurrentJetpackFuel;
+	}
+
+	float GetMaxJetpackFuel() const
+	{
+		return MaxJetpackFuel;
+	}
+
+	/** 서버에서 플레이어에게 제트팩을 지급한다. */
+	void GrantJetpack();
+
+	/** 서버에서 연료를 소비한다. */
+	bool ConsumeJetpackFuel(float Amount);
+
+protected:
+	/** 모든 플레이어가 알아야 하는 제트팩 보유 상태 */
+	UPROPERTY(
+		ReplicatedUsing = OnRep_HasJetpack,
+		VisibleAnywhere,
+		BlueprintReadOnly,
+		Category = "Player|Jetpack")
+	bool bHasJetpack = false;
+
+	/** 소유 플레이어 UI에서 사용할 현재 연료 */
+	UPROPERTY(
+		ReplicatedUsing = OnRep_JetpackFuel,
+		VisibleAnywhere,
+		BlueprintReadOnly,
+		Category = "Player|Jetpack")
+	float CurrentJetpackFuel = 0.f;
+
+	/** 프로토타입에서는 모든 인스턴스가 같은 기본값을 사용한다. */
+	UPROPERTY(
+		EditDefaultsOnly,
+		BlueprintReadOnly,
+		Category = "Player|Jetpack")
+	float MaxJetpackFuel = 100.f;
+
+	UFUNCTION()
+	void OnRep_HasJetpack();
+
+	UFUNCTION()
+	void OnRep_JetpackFuel();
+
+private:
+	/** 연결된 Pawn의 제트팩 외형을 현재 상태에 맞게 갱신한다. */
+	void RefreshJetpackVisualOnPawn();
+};
