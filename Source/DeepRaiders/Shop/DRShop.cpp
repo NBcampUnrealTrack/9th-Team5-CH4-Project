360ab0d9cdb983e96f13b3ba0fc7fbcd7f571b84
dotori <a01062688790@gmail.com>
2026-08-20 19:12:43 +0900

    refac : UI정리

--- Source/DeepRaiders/Shop/DRShop.cpp
+++ Source/DeepRaiders/Shop/DRShop.cpp
@@ -14,7 +14,7 @@ ADRShop::ADRShop()
 	PrimaryActorTick.bCanEverTick = false;
 	bReplicates = true;
 
-	// 상점 액터는 접근 범위와 UI 기능을 컴포넌트로 구성합니다.
+	// 상점 액터는 접근 범위와 거래 기능을 컴포넌트로 구성합니다.
 	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
 	SetRootComponent(Root);
 
