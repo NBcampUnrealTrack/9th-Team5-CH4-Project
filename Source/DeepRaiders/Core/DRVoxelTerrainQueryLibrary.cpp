cef8f19862fbbec70cf47de15e526b457658c51e
netter36 <netter36@naver.com>
2026-08-20 10:28:48 +0900

    feat: 최적화중

--- Source/DeepRaiders/Core/DRVoxelTerrainQueryLibrary.cpp
+++ Source/DeepRaiders/Core/DRVoxelTerrainQueryLibrary.cpp
@@ -5,6 +5,7 @@
 #include "VoxelMaterial.h"
 #include "VoxelTools/VoxelDataTools.h"
 #include "VoxelWorld.h"
+#include "VoxelTools/VoxelBlueprintLibrary.h"
 
 bool UDRVoxelTerrainQueryLibrary::GetMaterialCountsInBox(
 	AVoxelWorld* VoxelWorld,
@@ -150,20 +151,44 @@ bool UDRVoxelTerrainQueryLibrary::AddSnowInBox(
 		return false;
 	}
 
-	const FVector Min = BoxCenter - AbsExtent;
-	const FVector Max = BoxCenter + AbsExtent;
+	const FVector WorldMin = BoxCenter - AbsExtent;
+	const FVector WorldMax = BoxCenter + AbsExtent;
+
+	const FIntVector VoxelA = VoxelWorld->GlobalToLocal(WorldMin);
+	const FIntVector VoxelB = VoxelWorld->GlobalToLocal(WorldMax);
+
+	const FIntVector VoxelMin(
+		FMath::Min(VoxelA.X, VoxelB.X),
+		FMath::Min(VoxelA.Y, VoxelB.Y),
+		FMath::Min(VoxelA.Z, VoxelB.Z));
+
+	const FIntVector VoxelMax(
+		FMath::Max(VoxelA.X, VoxelB.X),
+		FMath::Max(VoxelA.Y, VoxelB.Y),
+		FMath::Max(VoxelA.Z, VoxelB.Z));
+
+	const int32 VoxelSampleStep = FMath::Max(
+		1,
+		FMath::RoundToInt(SampleStep / VoxelWorld->VoxelSize));
+
+	const int32 PatchRadius = FMath::Max(0, VoxelSampleStep / 2);
 
 	FVoxelMaterial SnowMaterial;
 	SnowMaterial.SetSingleIndex(SnowMaterialIndex);
 
-	for (float X = Min.X; X <= Max.X; X += SampleStep)
+	TSet<FIntVector> SnowVoxelPositions;
+	SnowVoxelPositions.Reserve(
+		((VoxelMax.X - VoxelMin.X) / VoxelSampleStep + 1) *
+		((VoxelMax.Y - VoxelMin.Y) / VoxelSampleStep + 1) *
+		FMath::Square(PatchRadius * 2 + 1));
+
+	for (int32 X = VoxelMin.X; X <= VoxelMax.X; X += VoxelSampleStep)
 	{
-		for (float Y = Min.Y; Y <= Max.Y; Y += SampleStep)
+		for (int32 Y = VoxelMin.Y; Y <= VoxelMax.Y; Y += VoxelSampleStep)
 		{
-			for (float Z = Max.Z; Z >= Min.Z; Z -= SampleStep)
+			for (int32 Z = VoxelMax.Z; Z >= VoxelMin.Z; --Z)
 			{
-				const FIntVector SurfaceVoxelPosition =
-					VoxelWorld->GlobalToLocal(FVector(X, Y, Z));
+				const FIntVector SurfaceVoxelPosition(X, Y, Z);
 
 				float SurfaceValue = 0.f;
 				UVoxelDataTools::GetValue(
@@ -171,40 +196,74 @@ bool UDRVoxelTerrainQueryLibrary::AddSnowInBox(
 					VoxelWorld,
 					SurfaceVoxelPosition);
 
-				// Voxel Plugin 1.2 기준으로 Value <= 0 이 solid.
 				if (SurfaceValue > 0.f)
 				{
 					continue;
 				}
 
-				const FIntVector SnowVoxelPosition =
-					SurfaceVoxelPosition + FIntVector(0, 0, 1);
+				const int32 SnowZ = Z + 1;
+				if (SnowZ > VoxelMax.Z)
+				{
+					break;
+				}
 
-				float CurrentSnowValue = 0.f;
-				UVoxelDataTools::GetValue(
-					CurrentSnowValue,
-					VoxelWorld,
-					SnowVoxelPosition);
+				for (int32 PatchX = X - PatchRadius; PatchX <= X + PatchRadius; ++PatchX)
+				{
+					if (PatchX < VoxelMin.X || PatchX > VoxelMax.X)
+					{
+						continue;
+					}
 
-				const float NewSnowValue = FMath::Clamp(
-					CurrentSnowValue - SnowAmount,
-					-1.f,
-					1.f);
+					for (int32 PatchY = Y - PatchRadius; PatchY <= Y + PatchRadius; ++PatchY)
+					{
+						if (PatchY < VoxelMin.Y || PatchY > VoxelMax.Y)
+						{
+							continue;
+						}
 
-				UVoxelDataTools::SetValue(
-					VoxelWorld,
-					SnowVoxelPosition,
-					NewSnowValue);
+						SnowVoxelPositions.Add(FIntVector(PatchX, PatchY, SnowZ));
+					}
+				}
 
-				UVoxelDataTools::SetMaterial(
-					VoxelWorld,
-					SnowVoxelPosition,
-					SnowMaterial);
-
-				OutModifiedVoxelCount++;
 				break;
 			}
 		}
+	}
+
+	for (const FIntVector& SnowVoxelPosition : SnowVoxelPositions)
+	{
+		float CurrentSnowValue = 0.f;
+		UVoxelDataTools::GetValue(
+			CurrentSnowValue,
+			VoxelWorld,
+			SnowVoxelPosition);
+
+		const float NewSnowValue = FMath::Clamp(
+			CurrentSnowValue - SnowAmount,
+			-1.f,
+			1.f);
+
+		UVoxelDataTools::SetValue(
+			VoxelWorld,
+			SnowVoxelPosition,
+			NewSnowValue);
+
+		UVoxelDataTools::SetMaterial(
+			VoxelWorld,
+			SnowVoxelPosition,
+			SnowMaterial);
+
+		OutModifiedVoxelCount++;
+	}
+
+	if (OutModifiedVoxelCount > 0)
+	{
+		const FVoxelIntBox UpdateBounds =
+			FVoxelIntBox(VoxelMin, VoxelMax).Extend(2);
+
+		UVoxelBlueprintLibrary::UpdateBounds(
+			VoxelWorld,
+			UpdateBounds);
 	}
 
 	return true;
