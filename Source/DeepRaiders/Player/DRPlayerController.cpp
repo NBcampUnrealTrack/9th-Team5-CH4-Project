f811c877a474a8d6568f1071b7410611b94e088a
Shin Dain <qwe3546@naver.com>
2026-08-21 20:06:22 +0900

    feat : 원거리 무기 Resource 타입에 따른 Cost 처리 추가, PlayerController 테스트 아이템 변수 추가

--- Source/DeepRaiders/Player/DRPlayerController.cpp
+++ Source/DeepRaiders/Player/DRPlayerController.cpp
@@ -364,6 +364,20 @@ void ADRPlayerController::InitializeStartingQuickSlot()
 		InventoryComponent->TryAddItemToSlot(1, StartingProjectileWeaponDefinition, 1);
 	}
 	
+#ifdef UE_BUILD_DEBUG
+	
+	if (!InventoryComponent->GetItemAtSlot(2))
+	{
+		InventoryComponent->TryAddItemToSlot(2, TestItemDefinition1, TestItemQuantity1);
+	}
+	
+	if (!InventoryComponent->GetItemAtSlot(3))
+	{
+		InventoryComponent->TryAddItemToSlot(3, TestItemDefinition2, TestItemQuantity2);
+	}
+	
+#endif
+	
 	QuickSlotComponent->RequestSelectSlot(0);	
 }
 
