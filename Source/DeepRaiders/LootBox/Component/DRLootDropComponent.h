e306faae7ff5b04a0877206ce2e5efd2705e584d
Shin Dain <qwe3546@naver.com>
2026-08-26 16:19:54 +0900

    feat : BreakableActor를 상속받은 LootBoxActor 클래스 추가, 랜덤 아이템 드랍 컴포넌트 LootDropComponent 추가

---
+++ Source/DeepRaiders/LootBox/Component/DRLootDropComponent.h
@@ -0,0 +1,58 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "DeepRaiders/LootBox/DRLootTypes.h"
+#include "DRLootDropComponent.generated.h"
+
+class DRItemDefinition;
+class UDRLootDropProfile;
+
+UCLASS(ClassGroup = (Loot), meta = (BlueprintSpawnableComponent))
+class DEEPRAIDERS_API UDRLootDropComponent : public UActorComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UDRLootDropComponent();
+	
+	/* 서버에서 Loot를 추첨하고 WorldItem으로 생성한다.
+	 * 반환값은 생성에 성공한 WorldItemActor 개수
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category = "Loot")
+	int32 GenerateAndSpawnLoot(EDRLootTier LootTier, const FTransform& SourceTransform);
+	
+	// 고정 시드 테스트용 C++ 전용 오버로드
+	int32 GenerateAndSpawnLoot(EDRLootTier LootTier, const FTransform& SourceTransform, FRandomStream& RandomStream);
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Loot")
+	TObjectPtr<UDRLootDropProfile> LootProfile = nullptr;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Loot|Spawn", meta = (ClampMin = "0.0", Units = "cm"))
+	float MinSpawnRadius = 100.f;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Loot|Spawn", meta = (ClampMin = "0.0", Units = "cm"))
+	float MaxSpawnRadius = 180.f;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Loot|Spawn", meta = (ClampMin = "0.0", Units = "deg"))
+	float MaxSpawnAngleJitterDegrees = 10.f;
+	
+private:
+	bool BuildValidLootPools(TArray<const FDRLootTableRow*>& OutAllRows,
+		TMap<EDRItemRarity, TArray<const FDRLootTableRow*>>& OutRowsByRarity) const;
+	
+	bool RollLootQuantities(EDRLootTier LootTier, const FDRLootTierConfig& TierConfig,
+		const TArray<const FDRLootTableRow*>& AllRows,
+		const TMap<EDRItemRarity, TArray<const FDRLootTableRow*>>& RowsByRarity,
+		FRandomStream& RandomStream, TMap<UDRItemDefinition*, int32>& OutGeneratedQuantities)const;
+	
+	int32 SpawnLootQuantities(const TMap<UDRItemDefinition*, int32>& GeneratedQuantities,
+		const FTransform& SourceTransform,
+		FRandomStream& RandomStream) const;
+	
+	bool SelectRarity(const FDRLootTierConfig& TierConfig, FRandomStream& RandomStream, EDRItemRarity& OutRarity) const;
+	
+	const FDRLootTableRow* SelectWeightedLootRow(const TArray<const FDRLootTableRow*>& CandidateRows,
+		FRandomStream& RandomStream) const;
+};
---
+++ Source/DeepRaiders/LootBox/DRLootBoxActor.cpp
@@ -0,0 +1,77 @@
+﻿
+#include "DRLootBoxActor.h"
+
+#include "Components/SceneComponent.h"
+#include "DeepRaiders/LootBox/Component/DRLootDropComponent.h"
+#include "Net/UnrealNetwork.h"
+
+ADRLootBoxActor::ADRLootBoxActor()
+{
+	LootDropComponent = CreateDefaultSubobject<UDRLootDropComponent>(TEXT("LootDropComponent"));
+	LootSpawnPointComponent = CreateDefaultSubobject<USceneComponent>(TEXT("LootSpawnPointComponent"));
+	
+	LootSpawnPointComponent->SetupAttachment(GetRootComponent());
+}
+
+void ADRLootBoxActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (GetNetMode() != NM_DedicatedServer)
+	{
+		BP_OnLootTierChanged(LootTier);
+	}
+}
+
+void ADRLootBoxActor::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME(ThisClass, LootTier);
+}
+
+bool ADRLootBoxActor::SetLootTier(EDRLootTier NewLootTier)
+{
+	if (!HasAuthority()
+		|| IsBroken())
+	{
+		return false;
+	}
+	
+	if (LootTier == NewLootTier)
+	{
+		return true;
+	}
+	
+	LootTier = NewLootTier;
+	ForceNetUpdate();
+	
+	if (GetNetMode() != NM_DedicatedServer)
+	{
+		BP_OnLootTierChanged(LootTier);
+	}
+	
+	return true;
+}
+
+void ADRLootBoxActor::HandleBroken(const FDRBreakableDamageContext& DamageContext)
+{
+	Super::HandleBroken(DamageContext);
+	
+	if (!HasAuthority()
+		|| !IsValid(LootDropComponent)
+		|| !IsValid(LootSpawnPointComponent))
+	{
+		return;
+	}
+	
+	LootDropComponent->GenerateAndSpawnLoot(LootTier, LootSpawnPointComponent->GetComponentTransform());
+}
+
+void ADRLootBoxActor::OnRep_LootTier()
+{
+	if (GetNetMode() != NM_DedicatedServer)
+	{
+		BP_OnLootTierChanged(LootTier);
+	}
+}
