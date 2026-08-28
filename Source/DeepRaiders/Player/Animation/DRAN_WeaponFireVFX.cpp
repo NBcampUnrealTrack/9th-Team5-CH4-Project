71109cfdcc734829680bf122fdb685d30ced8bee
SeungMinLee <mealh333@gmail.com>
2026-08-29 02:41:37 +0900

    feat: weapon vfx 실행 방식 AN으로 옮기기

---
+++ Source/DeepRaiders/Player/Animation/DRAN_WeaponFireVFX.cpp
@@ -0,0 +1,26 @@
+﻿#include "DRAN_WeaponFireVFX.h"
+
+#include "Components/SkeletalMeshComponent.h"
+#include "DeepRaiders/Player/DRPlayerCharacter.h"
+
+void UDRAN_WeaponFireVFX::Notify(
+	USkeletalMeshComponent* MeshComp,
+	UAnimSequenceBase* Animation,
+	const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	if (!IsValid(MeshComp))
+	{
+		return;
+	}
+
+	ADRPlayerCharacter* Character = Cast<ADRPlayerCharacter>(MeshComp->GetOwner());
+
+	if (!IsValid(Character) || Character->GetNetMode() == NM_DedicatedServer)
+	{
+		return;
+	}
+
+	Character->PlayProjectileFireVFXFromNotify();
+}
---
+++ Source/DeepRaiders/Player/Animation/DRAN_WeaponFireVFX.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "DRAN_WeaponFireVFX.generated.h"
+
+UCLASS(meta = (DisplayName = "DRAN_WeaponFireVFX"))
+class DEEPRAIDERS_API UDRAN_WeaponFireVFX
+	: public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Notify(
+		USkeletalMeshComponent* MeshComp,
+		UAnimSequenceBase* Animation,
+		const FAnimNotifyEventReference& EventReference) override;
+};
