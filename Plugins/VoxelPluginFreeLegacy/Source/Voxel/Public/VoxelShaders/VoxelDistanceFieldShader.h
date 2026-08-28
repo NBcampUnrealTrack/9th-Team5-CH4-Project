949f8c3183f1ff6eafc1ed062c2f5543c6e5fff5
kimjinkang <wlsrkd99@naver.com>
2026-08-28 18:25:01 +0900

    fix: RDG 기반 Voxel JumpFlood GPU 동기화 적용
    - JumpFlood compute pass를 수동 RHI buffer 관리에서 RDG로 전환
    - Src SRV / Dst UAV 의존성을 명시해 GPU barrier를 안전하게 처리
    - GPU readback staging buffer를 사용해 결과 회수를 안정화
    - CPU/GPU invalid surface position 기준을 일치시켜 거리 필드 판정 오류 수정

--- Plugins/VoxelPluginFreeLegacy/Source/Voxel/Public/VoxelShaders/VoxelDistanceFieldShader.h
+++ Plugins/VoxelPluginFreeLegacy/Source/Voxel/Public/VoxelShaders/VoxelDistanceFieldShader.h
@@ -4,112 +4,57 @@
 
 #include "CoreMinimal.h"
 #include "VoxelMinimal.h"
-#include "ShaderParameterMacros.h"
-#include "RenderCommandFence.h"
 #include "GlobalShader.h"
-
-struct FRHIBatchedShaderParameters;
-struct FRHIBatchedShaderUnbinds;
-
-BEGIN_GLOBAL_SHADER_PARAMETER_STRUCT(FVoxelDistanceFieldParameters,)
-
-	SHADER_PARAMETER(uint32, SizeX)
-	SHADER_PARAMETER(uint32, SizeY)
-	SHADER_PARAMETER(uint32, SizeZ)
-	SHADER_PARAMETER(uint32, Step)
-
-END_GLOBAL_SHADER_PARAMETER_STRUCT()
+#include "RenderCommandFence.h"
+#include "RenderGraphResources.h"
+#include "ShaderParameterStruct.h"
 
 #define VOXEL_DISTANCE_FIELD_NUM_THREADS_CS 8
 
-typedef TUniformBufferRef<FVoxelDistanceFieldParameters> FVoxelDistanceFieldParametersRef;
-
-class FVoxelDistanceFieldBaseCS : public FGlobalShader
+class FVoxelJumpFloodCS : public FGlobalShader
 {
-	DECLARE_TYPE_LAYOUT(FVoxelDistanceFieldBaseCS, NonVirtual);
-public:
-	FVoxelDistanceFieldBaseCS() = default;
-	FVoxelDistanceFieldBaseCS(const ShaderMetaType::CompiledShaderInitializerType& Initializer);
+	DECLARE_GLOBAL_SHADER(FVoxelJumpFloodCS);
+	SHADER_USE_PARAMETER_STRUCT(FVoxelJumpFloodCS, FGlobalShader);
 
-	static bool ShouldCache(EShaderPlatform Platform)
-	{
-		return IsFeatureLevelSupported(Platform, ERHIFeatureLevel::SM5);
-	}
+public:
+	BEGIN_SHADER_PARAMETER_STRUCT(FParameters, )
+		SHADER_PARAMETER(uint32, SizeX)
+		SHADER_PARAMETER(uint32, SizeY)
+		SHADER_PARAMETER(uint32, SizeZ)
+		SHADER_PARAMETER(uint32, Step)
+		SHADER_PARAMETER_RDG_BUFFER_SRV(Buffer<float>, Src)
+		SHADER_PARAMETER_RDG_BUFFER_UAV(RWBuffer<float>, Dst)
+	END_SHADER_PARAMETER_STRUCT()
+
 	static bool ShouldCompilePermutation(const FGlobalShaderPermutationParameters& Parameters)
 	{
 		return IsFeatureLevelSupported(Parameters.Platform, ERHIFeatureLevel::SM5);
 	}
-	static void ModifyCompilationEnvironment(const FGlobalShaderPermutationParameters& Parameters, FShaderCompilerEnvironment& OutEnvironment);
-	
-	void SetBuffers(
-		FRHIBatchedShaderParameters& BatchedParameters,
-		const FRWBuffer& SrcBuffer,
-		const FRWBuffer& DstBuffer) const;
 
-	void SetUniformBuffers(
-		FRHIBatchedShaderParameters& BatchedParameters,
-		const FVoxelDistanceFieldParameters& Parameters) const;
-
-	void UnsetBuffers(FRHIBatchedShaderUnbinds& BatchedUnbinds) const;
-
-private:
-#if VOXEL_ENGINE_VERSION >= 503
-	LAYOUT_FIELD(FShaderResourceParameter, Src);
-	LAYOUT_FIELD(FShaderResourceParameter, Dst);
-#else
-	PRAGMA_DISABLE_DEPRECATION_WARNINGS
-	LAYOUT_FIELD(FRWShaderParameter, Src);
-	LAYOUT_FIELD(FRWShaderParameter, Dst);
-	PRAGMA_ENABLE_DEPRECATION_WARNINGS
-#endif
+	static void ModifyCompilationEnvironment(
+		const FGlobalShaderPermutationParameters& Parameters,
+		FShaderCompilerEnvironment& OutEnvironment);
 };
-
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-
-class FVoxelJumpFloodCS : public FVoxelDistanceFieldBaseCS
-{
-public:
-    DECLARE_SHADER_TYPE(FVoxelJumpFloodCS, Global);
-
-	using FVoxelDistanceFieldBaseCS::FVoxelDistanceFieldBaseCS;
-};
-
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
 
 class VOXEL_API FVoxelDistanceFieldShaderHelper : public TVoxelSharedFromThis<FVoxelDistanceFieldShaderHelper>
 {
 public:
 	FVoxelDistanceFieldShaderHelper() = default;
-	
+
 	void WaitForCompletion() const;
 
 	void StartCompute(
-        const FIntVector& Size,
-        const TVoxelSharedRef<TArray<FVector3f>>& InOutData, 
+		const FIntVector& Size,
+		const TVoxelSharedRef<TArray<FVector3f>>& InOutData,
 		int32 MaxPasses_Debug = -1);
 
 	void Compute_RenderThread(
-        FRHICommandListImmediate& RHICmdList,
-        const FIntVector& Size,
-        FVector3f* RESTRICT Data,
+		FRHICommandListImmediate& RHICmdList,
+		const FIntVector& Size,
+		FVector3f* RESTRICT Data,
 		int32 Num,
 		int32 MaxPasses_Debug = -1);
 
 private:
-	FIntVector AllocatedSize = FIntVector::ZeroValue;
-	
-	FRWBuffer SrcBuffer;
-	FRWBuffer DstBuffer;
-
 	FRenderCommandFence Fence;
-
-	template<typename T>
-	void ApplyComputeShader(
-		FRHICommandListImmediate& RHICmdList, 
-		const FIntVector& Size,
-		int32 Step);
 };
