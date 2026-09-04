ef5dda535e3b2635b6e0e03e1af412c67792a806
netter36 <netter36@naver.com>
2026-09-04 20:11:43 +0900

    test: 서버 주소 변경

--- Source/DeepRaiders/UI/Title/HostOrJoinWidget.h
+++ Source/DeepRaiders/UI/Title/HostOrJoinWidget.h
@@ -59,5 +59,6 @@ protected:
 	TObjectPtr<UDRTitleMapChoiceWidget> WBP_ChoiceMap;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Session")
-	FString DedicatedServerAddress = TEXT("shees95.myddns.me:17777");
+	// FString DedicatedServerAddress = TEXT("shees95.myddns.me:17777");
+	FString DedicatedServerAddress = TEXT("katherine-fc.tun.ply.gg:55341");
 };
