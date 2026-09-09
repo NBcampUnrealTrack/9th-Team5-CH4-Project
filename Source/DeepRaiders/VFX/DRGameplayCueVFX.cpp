22d5f7ce28c70d4a860eb7c9d78dbb64e25e6c17
dotori <a01062688790@gmail.com>
2026-09-09 14:48:00 +0900

    feat : 색적스킬 vfx 추가
    - 로컬에서 보이게만 해둠.
    - 색적GA에서 VFX에서 모두 보이게 변경가능

--- Source/DeepRaiders/VFX/DRGameplayCueVFX.cpp
+++ Source/DeepRaiders/VFX/DRGameplayCueVFX.cpp
@@ -76,6 +76,7 @@ bool UDRGameplayCueVFX::BuildRequest(AActor* Target, const FGameplayCueParameter
 
 	OutRequest.TargetActor = Target;
 	OutRequest.VFXTag = VFXTag;
+	OutRequest.RawMagnitude = Parameters.RawMagnitude;
 
 	return true;
 }
